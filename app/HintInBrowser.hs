f87c2d141aaac1ccabd8382fdf4033349b36eba4
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-06-25 01:51:50 +0100

    Hint demo works

--- app/HintInBrowser.hs
+++ app/HintInBrowser.hs
@@ -1,6 +1,15 @@
 module HintInBrowser (runHintInBrowser) where
 
+import Language.Haskell.Interpreter
+import Language.Haskell.Interpreter.Unsafe
+import Control.Exception
+import System.IO
+
 foreign export javascript "run_hint_in_browser" runHintInBrowser :: IO ()
 
 runHintInBrowser :: IO ()
-runHintInBrowser = putStrLn "Hello, Haskell!"
+runHintInBrowser = do
+  res <- unsafeRunInterpreterWithArgs ["-package-env", "/tmp/ghc_env"] $ do
+    setImports ["Prelude"]
+    interpret "'x'" (error "witness" :: Char)
+  print res
