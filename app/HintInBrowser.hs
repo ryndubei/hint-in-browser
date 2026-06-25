f81851b247ee8ecc368e15dfd9da62b503d2638e
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-06-25 02:32:48 +0100

    Demo with extra hackage dependencies

--- app/HintInBrowser.hs
+++ app/HintInBrowser.hs
@@ -2,14 +2,17 @@ module HintInBrowser (runHintInBrowser) where
 
 import Language.Haskell.Interpreter
 import Language.Haskell.Interpreter.Unsafe
-import Control.Exception
-import System.IO
+import qualified Data.Vector as V
 
 foreign export javascript "run_hint_in_browser" runHintInBrowser :: IO ()
 
 runHintInBrowser :: IO ()
 runHintInBrowser = do
   res <- unsafeRunInterpreterWithArgs ["-package-env", "/tmp/ghc_env"] $ do
-    setImports ["Prelude"]
-    interpret "'x'" (error "witness" :: Char)
+    setImportsF
+      [ ModuleImport "Prelude" NotQualified NoImportList
+      , ModuleImport "Data.Vector" (QualifiedAs (Just "V")) NoImportList
+      , ModuleImport "Data.Vector" NotQualified (ImportList ["Vector"])
+      ]
+    interpret "V.fromList [1,2,3]" (error "witness" :: V.Vector Int)
   print res
