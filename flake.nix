50bb6bdd6ae99756155cba975201c500eed2f02f
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-06-26 01:03:50 +0100

    Build bsdtar for wasm in nix

--- flake.nix
+++ flake.nix
@@ -19,9 +19,14 @@
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [ ghc-wasm-meta.packages.${system}.all_9_14 ];
       };
-      packages.x86_64-linux.pkgsCross.wasi32.zstd-wasm = pkgs.callPackage ./zstd-wasm.nix {
-        ghc-wasm-meta = ghc-wasm-meta.packages.${system}.all_9_14;
+      packages.x86_64-linux.pkgsCross.wasi32 = rec {
+        zstd-wasm = pkgs.callPackage ./zstd-wasm.nix {
+          ghc-wasm-meta = ghc-wasm-meta.packages.${system}.all_9_14;
+        };
+        bsdtar-wasm = pkgs.callPackage ./bsdtar-wasm.nix {
+          inherit zstd-wasm;
+          ghc-wasm-meta = ghc-wasm-meta.packages.${system}.all_9_14;
+        };
       };
-      packages.x86_64-linux.pkgsCross.wasi32.bsdtar-wasm = abort "todo";
     };
 }
